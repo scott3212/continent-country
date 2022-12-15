fabf461e9ff4a3d624e83a8fcd02c421b95dd371
Scott Wu <scott3212@gmail.com>
2022-12-14 22:48:46 -0500

    Initial Commit
    - Upgrade sprintboot version to latest 3.0.0
    - Fix compilation error caused by springboot version upgrade

---
+++ src/test/java/com/example/takehome/TakehomeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.takehome;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.actuate.endpoint.ApiVersion;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakehomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(ApiVersion.LATEST.equals(ApiVersion.V3));
+	}
+}
